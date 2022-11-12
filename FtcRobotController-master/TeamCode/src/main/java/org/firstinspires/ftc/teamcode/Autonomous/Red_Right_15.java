5404b1ebb6a59c9d50ef05293bbe125b6f0b0cf2
FTCRoborioles <FTCRoborioles2021@gmail.com>
2022-11-12 11:29:05 -0500

    Adjusted Autonomous
    I slightly adjusted autonomous due to the realization we will at no point be turning.

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Red_Right_15.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Red_Right_15.java
@@ -12,112 +12,104 @@ public class Red_Right_15 extends org.firstinspires.ftc.teamcode.Autonomous.Auto
         //add method below that takes signal cone color
 
 
-        //moves away from wall, turns, scores on medium junction, and picks up new cone
-        move(y, 0.1);
+        //moves away from wall, scores on medium junction, and picks up new cone
         moveElevator(2);
-        turn(-1);
-        move(x, 2);
-        move(y, 0.5);
+        move(y, 1.6);
         pivot(-1);
         openGripper();
-        pivot(0);
+        move(y, 0.5);
+        pivot(1);
         moveElevator(0);
-        move(y, -1.5);
+        move(x, -1.5);
 
         //takes which parking spot the signal cone wants to be parked in (3 being the closest slot to the wall and 1 being the closest to the middle of the field)
         if (parkingSlot == 3) {
             //scores cone on medium junction
             moveElevator(2);
-            move(y, 1.5);
-            pivot(1);
+            move(x, -1.5);
+            pivot(0);
             openGripper();
 
             //picks up another cone
-            pivot(0);
+            pivot(1);
             moveElevator(0);
-            move(y, -1.5);
+            move(x, 1.5);
             moveElevator(1);
 
             //scores cone on low junction
-            move(y, 0.5);
-            pivot(1);
-            openGripper();
+            move(x, -0.5);
             pivot(0);
+            openGripper();
 
             //parks in slot 3
-            move(y, -0.5);
-            move(x, 1);
+            move(x, 0.5);
+            move(y, -1.5);
         }
         else if (parkingSlot == 2) {
             //scores cone on low junction
             moveElevator(1);
-            move(y, 0.5);
-            pivot(1);
+            move(x, -0.5);
+            pivot(0);
             openGripper();
 
             //picks up another cone
-            pivot(0);
+            pivot(1);
             moveElevator(0);
-            move(y, -0.5);
+            move(x, 0.5);
             moveElevator(2);
 
             //scores cone on medium junction
-            move(y, 1.5);
-            pivot(1);
-            openGripper();
+            move(x, -1.5);
             pivot(0);
+            openGripper();
 
             //parks in slot 2
-            move(y, 0.5);
-            move(x, 1);
+            move(x, -0.5);
+            move(y, -1.5);
         }
         else if (parkingSlot == 1) {
             //scores cone on medium junction
             moveElevator(2);
-            move(y, 1.5);
-            pivot(1);
+            move(x, -1.5);
+            pivot(0);
             openGripper();
 
             //picks up another cone
-            pivot(0);
+            pivot(1);
             moveElevator(0);
-            move(y, -1.5);
+            move(x, 1.5);
             moveElevator(1);
 
             //scores cone on low junction
-            move(y, 0.5);
-            pivot(1);
-            openGripper();
+            move(x, -0.5);
             pivot(0);
+            openGripper();
 
             //parks in slot 1
-            move(y, 0.5);
-            move(x, 1);
+            move(x, -0.5);
+            move(y, -1.5);
         }
         else {
-            //backup in case camera doesn't identify cone color and parks in middle slot
-
-            //scores cone on medium junction
-            moveElevator(2);
-            move(y, 1.5);
-            pivot(1);
+            //scores cone on low junction
+            moveElevator(1);
+            move(x, -0.5);
+            pivot(0);
             openGripper();
 
             //picks up another cone
-            pivot(0);
-            moveElevator(0);
-            move(y, -1.5);
-            moveElevator(1);
-
-            //scores cone on low junction
-            move(y, 0.5);
             pivot(1);
-            openGripper();
+            moveElevator(0);
+            move(x, 0.5);
+            moveElevator(2);
+
+            //scores cone on medium junction
+            move(x, -1.5);
             pivot(0);
+            openGripper();
 
             //parks in slot 2
-            move(y, 0.5);
-            move(x, 1);
+            move(x, -0.5);
+            move(y, -1.5);
         }
     }
 */
