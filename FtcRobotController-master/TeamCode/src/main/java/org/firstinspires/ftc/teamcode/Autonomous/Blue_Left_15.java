b5a36bc2de332e11acea6e6daf6f44a82dd8009e
FTCRoborioles <FTCRoborioles2021@gmail.com>
2022-11-15 13:34:11 -0500

    -

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Blue_Left_15.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Blue_Left_15.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 public class Blue_Left_15 extends org.firstinspires.ftc.teamcode.Autonomous.Auto_Base_15 {
 
     public void runOpMode() throws InterruptedException {
+        lift.auto_lift_set(camera.zone());
+
 
     }
 
--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Call_Upon_Classes/New.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Call_Upon_Classes/New.java
@@ -1,39 +1,137 @@
-package org.firstinspires.ftc.teamcode.Call_Upon_Classes;
-
-
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-@Disabled
-public class New extends LinearOpMode {
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-
-
-        boolean x = true;
-        int y = 1;
-        double d = 0.5;
-        DcMotor motor;
-        Servo servo;
-        CRServo CRServo;
-
-        while (x){
-
-            if(x){
-
-
-            }else{
-
-            }
-
-        }
-
-
-    }
-}
+//package org.firstinspires.ftc.teamcode.Call_Upon_Classes;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+//import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+//import org.openftc.apriltag.AprilTagDetection;
+//import org.openftc.easyopencv.OpenCvCamera;
+//import org.openftc.easyopencv.OpenCvCameraFactory;
+//import org.openftc.easyopencv.OpenCvCameraRotation;
+//import org.firstinspires.ftc.teamcode.Call_Upon_Classes.*;
+//
+//import java.util.ArrayList;
+//
+//
+//import android.hardware.Sensor;
+//import android.hardware.SensorManager;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.hardware.CRServo;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.Servo;
+//
+//import org.openftc.apriltag.AprilTagDetection;
+//
+//import java.util.ArrayList;
+//
+//public class New{
+//    OpenCvCamera camera;
+//    AprilTagDetectionPipeline pipeline;
+//
+//    static final double FEET_PER_METER = 3.28084;
+//
+//    // Lens intrinsics
+//    // UNITS ARE PIXELS
+//    // NOTE: this calibration is for the C920 webcam at 800x448.
+//    // You will need to do your own calibration for other configurations!
+//    double fx = 578.272;
+//    double fy = 578.272;
+//    double cx = 402.145;
+//    double cy = 221.506;
+//
+//    // UNITS ARE METERS
+//    double tagsize = 0.166;
+//
+//    // Tag ID 1, 2, 3  from the 36h11 family
+//    int LEFT = 1;
+//    int MIDDLE = 2;
+//    int RIGHT = 3;
+//
+//    public void init(){
+//        auto_wheels.init_auto_drive_motors(hardwareMap, telemetry);
+//        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+//        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam"), cameraMonitorViewId);
+//        pipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+//
+//        camera.setPipeline(pipeline);
+//        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+//        {
+//            @Override
+//            public void onOpened()
+//            {
+//                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+//            }
+//
+//            @Override
+//            public void onError(int errorCode)
+//            {
+//
+//            }
+//        });
+//    }
+//
+//    AprilTagDetection tagOfInterest = null;
+//    public void camera(){
+//        while (!isStarted() && !isStopRequested())
+//        {
+//            ArrayList<AprilTagDetection> currentDetections = pipeline.getLatestDetections();
+//
+//            if(currentDetections.size() != 0)
+//            {
+//                boolean tagFound = false;
+//
+//                for(AprilTagDetection tag : currentDetections)
+//                {
+//                    if(tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT)
+//                    {
+//                        tagOfInterest = tag;
+//                        tagFound = true;
+//                        break;
+//                    }
+//                }
+//
+//                if(tagFound)
+//                {
+//                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+//                    tagToTelemetry(tagOfInterest);
+//                }
+//                else
+//                {
+//                    telemetry.addLine("Don't see tag of interest :(");
+//
+//                    if(tagOfInterest == null)
+//                    {
+//                        telemetry.addLine("(The tag has never been seen)");
+//                    }
+//                    else
+//                    {
+//                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+//                        tagToTelemetry(tagOfInterest);
+//                    }
+//                }
+//
+//            }
+//            else
+//            {
+//                telemetry.addLine("Don't see tag of interest :(");
+//
+//                if(tagOfInterest == null)
+//                {
+//                    telemetry.addLine("(The tag has never been seen)");
+//                }
+//                else
+//                {
+//                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+//                    tagToTelemetry(tagOfInterest);
+//                }
+//
+//            }
+//
+//            telemetry.update();
+//            sleep(20);
+//        }
+//    }
+//}
